c363f19c517fb1e59ef5450ea6baad6b361d768e
anasmunaf <muhammad.anas1997@gmail.com>
2021-07-19 04:30:20 +0500

    dependecies updated

--- startup/db.js
+++ startup/db.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function () {
+  // const db =
+  // "mongodb+srv://anas:0000@cluster0.ujgot.mongodb.net/VidlyApp?retryWrites=true&w=majority";
   const db = config.get("db");
   mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 };
