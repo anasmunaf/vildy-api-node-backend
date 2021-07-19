6849a58e935f07d50daa9651a55935ce907937be
anasmunaf <muhammad.anas1997@gmail.com>
2021-07-19 23:46:32 +0500

    updated error

--- startup/db.js
+++ startup/db.js
@@ -5,8 +5,6 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function () {
-  // const db =
-  // "mongodb+srv://anas:0000@cluster0.ujgot.mongodb.net/VidlyApp?retryWrites=true&w=majority";
   const db = config.get("db");
   mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 };
